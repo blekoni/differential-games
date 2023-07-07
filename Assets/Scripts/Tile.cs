3fbd86d9441e8c96f757def5216ca251467cb8bc
Andrii <blekoni420@gmail.com>
2023-07-07 11:25:23 +0300

    Tile on mouse move behaviour

--- Assets/Scripts/Tile.cs
+++ Assets/Scripts/Tile.cs
@@ -6,12 +6,29 @@ public class Tile : MonoBehaviour
 {
     [SerializeField] private Color m_baseColor, m_offsetColor;
     [SerializeField] private SpriteRenderer m_renderer;
+    bool m_bIsOffset = false;
 
     public void Init(bool isOffset)
     {
+        m_bIsOffset = isOffset;
+        SetColor();
+    }
+
+    private void SetColor()
+    {
         if (m_renderer)
         {
-            m_renderer.color = isOffset ? m_offsetColor : m_baseColor;
+            m_renderer.color = m_bIsOffset ? m_offsetColor : m_baseColor;
         }
+    }
+
+    private void OnMouseEnter()
+    {
+        m_renderer.color = Color.red;
+    }
+
+    private void OnMouseExit()
+    {
+        SetColor();
     }
 }
